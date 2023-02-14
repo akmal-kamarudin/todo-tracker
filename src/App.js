488ad1b7c9f302000c9c32dc9152c272f17131ae
Akmal Kamarudin <akmal.kamarudin19@gmail.com>
2023-02-14 23:21:45 +0800

    delete and add features

--- src/App.js
+++ src/App.js
@@ -22,21 +22,37 @@ const App = () => {
     const res = await fetch('http://localhost:5000/tasks')
     const data = await res.json()
 
-    console.log(data)
+    //console.log(data)
     return data
   }
   
 
   // Add Task
-  const addTask = (task) => {
-    const id = Math.floor(Math.random() * 10000) + 1
-    const newTask = { id, ...task }
-    setTasks([...tasks, newTask])
+  const addTask = async (task) => {
+    const res = await fetch('http://localhost:5000/tasks', {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(task)
+    })
+
+    const data = await res.json()
+
+    setTasks([...tasks, data])
+
+    // const id = Math.floor(Math.random() * 10000) + 1
+    // const newTask = { id, ...task }
+    // setTasks([...tasks, newTask])
     // console.log(id)
   }
 
   // Delete Task
-  const deleteTask = (id) => {
+  const deleteTask = async (id) => {
+    await fetch(`http://localhost:5000/tasks/${id}`, {
+      method: 'DELETE'
+    })
+
     setTasks(tasks.filter((task) => task.id !== id))
   }
 
