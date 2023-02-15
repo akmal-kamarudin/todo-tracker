fdf893adc485e3143904eec680d3735d30a96c58
Akmal Kamarudin <akmal.kamarudin19@gmail.com>
2023-02-15 22:21:17 +0800

    updated toggle reminder

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useState, useEffect } from 'react'
 import Header from './components/Header'
+import Footer from './components/Footer'
 import Tasks from './components/Tasks'
 import AddTask from './components/AddTask'
 
@@ -20,6 +21,15 @@ const App = () => {
   // Fetch Tasks
   const fetchTasks = async () => {
     const res = await fetch('http://localhost:5000/tasks')
+    const data = await res.json()
+
+    //console.log(data)
+    return data
+  }
+
+  // Fetch Tasks
+  const fetchTask = async (id) => {
+    const res = await fetch(`http://localhost:5000/tasks/${id}`)
     const data = await res.json()
 
     //console.log(data)
@@ -57,25 +67,45 @@ const App = () => {
   }
 
   // Toggle Reminder
-  const toggleReminder = (id) => {
+  const toggleReminder = async (id) => {
+    const taskToToggle = await fetchTask(id)
+    const updTask = { ...taskToToggle, reminder: !taskToToggle.reminder }
+
+    const res = await fetch(`http://localhost:5000/tasks/${id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(updTask)
+    })
+
+    const data = await res.json()
+
     setTasks(
       tasks.map((task) => 
         task.id === id ? { ...task, reminder: 
-          !task.reminder} : task
+          data.reminder} : task
       )
     )
   }
 
   return (
     <div className="container">
-      <Header onAdd={() => setShowAddTask
-        (!showAddTask)} showAdd={showAddTask}/>
+      <Header 
+        onAdd={() => setShowAddTask
+        (!showAddTask)} 
+        showAdd={showAddTask}
+      />
       {showAddTask && <AddTask onAdd={addTask} />}
       {tasks.length > 0 ? (
-        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
+        <Tasks tasks={tasks} 
+        onDelete={deleteTask} 
+        onToggle={toggleReminder} 
+        />
       ) : (
         'No Tasks to show'
       )}
+      <Footer />
     </div>
   );
 }
